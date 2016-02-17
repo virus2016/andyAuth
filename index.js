cdce259f9906fad5c24f106e4950b53880e292bc
Andrew Thompson <a.thompson@craneware.com>
2016-02-17 14:05:58 +0000

    Signed-off-by: Andrew Thompson <a.thompson@craneware.com>

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    oauthserver = require('oauth2-server');
+var oAuthModel = require('./model.js');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.oauth = oauthserver({
+    model: oAuthModel, // See below for specification
+    grants: ['password'],
+    debug: true
+});
+
+app.all('/oauth/token', app.oauth.grant());
+
+app.get('/', app.oauth.authorise(), function(req, res) {
+    res.send('Secret area');
+});
+
+app.use(app.oauth.errorHandler());
+
+app.listen(3000);
